2f823e8eefd08249d8700e20b32ffd7dc9fe22cb
Ruben Jongejan <ruben.jongejan@gmail.com>
2016-06-24 12:12:01 +0200

    Added sample selenium test.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.hamcrest.core.IsEqual;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,19 +16,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class SeleniumTest {
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         ChromeDriverManager.getInstance().setup();
     }
 
-
     @Test
-    public void seleniumTest(){
+    public void navigateToGoogleHomePageAndSearch() {
+//This line initializes the WebDriver
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com");
-        assertThat("On Google HomePage.", driver.getTitle(), equalTo("Google"));
+//This line gives the WebDriver the command to navigate to the Google homepage.
+        driver.get("http://google.com");
 
-        driver.quit();
+//Our first assertion. Let's see if we are on the Google HomePage.
+        assertThat("Title is 'Google'.", driver.getTitle(), IsEqual.equalTo("Google"));
+
+//This line defines by what the WebDriver will need to search the Element, as we'll see below.
+        By finder = By.name("q");
+
+//This line retrieves the WebElement from the page.
+        WebElement searchField = driver.findElement(finder);
+
+//The 'sendKeys' method of the WebElement is used to type the query in the search-field. We'll add \n to press Enter as well.
+        searchField.sendKeys("First test.\n");
+
+//Now it's time for an assertion. This will assert if the title is what we expect it to be. (It will fail ;))
+        assertThat("Title is as expected.", driver.getTitle(), containsString("First test. - Google"));
     }
+
 
 }
